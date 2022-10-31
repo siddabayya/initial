deb08f789b06cdf324ec2a7eb039ff1ae8556f9d
Sidda Reddy <siddareddy.sg@gmail.com>
2022-11-01 07:52:37 +0800

    Initial Commit

---
+++ src/main/java/com/myfood/repo/UserRepository.java
@@ -0,0 +1,8 @@
+package com.myfood.repo;
+
+import com.myfood.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+}
